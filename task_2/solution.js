b04dc7b0bea708d438c132892d8336171c00f49c
Natalia <smartaliant@gmail.com>
2023-04-17 19:47:16 +0300

    task_2 changed

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,7 +15,13 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+    let shippingSum = 0;
+    if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -34,6 +40,13 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
+    let discountSum;
+    if (productsSum >= discountMinSum) {
+        discountSum = productsSum * discountPart / 100;
+    } else {
+        discountSum = 0;  
+    }
+    
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -49,6 +62,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
+    let totalSum = productsSum - discountSum;
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
@@ -57,6 +71,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    totalSum = totalSum + shippingSum;
+    let freeShipping = shippingSum == 0;
 
     // Конец решения задачи №2.3.
 
