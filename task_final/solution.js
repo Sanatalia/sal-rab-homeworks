dda51928e9da2ecdb1f104157fe2e68d82bb11f8
Natalia <smartaliant@gmail.com>
2023-04-22 07:39:57 +0300

    task_final changed

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,14 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    return JSON.stringify({
+        data: {
+            client: `${name} ${phone}`, 
+            order: { 
+                address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`, 
+                sum
+            },
+            goods: goods.map(t => ({title: t.title, count: t.count}))
+        }
+    });
 }
